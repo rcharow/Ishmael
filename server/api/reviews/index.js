94bf0c92b5b3b726599b4a182942b102309e359e
Robbie Ferguson <robbieferguson139@gmail.com>
2015-07-13 10:57:01 -0400

    styling

--- server/api/reviews/index.js
+++ server/api/reviews/index.js
@@ -5,7 +5,7 @@ module.exports = router;
 
 router.get('/', function (req, res, next) {
 	// replace this temporary URL with deployed one for demonstration
-	rp('http://127.0.0.1:8080/api/run?id=55a2fd2516778ee607f19eaa')
+	rp('http://127.0.0.1:8080/api/run?id=55a3ce57928a01f407fd5019')
 	.then(function (data) {
 		res.json(data);
 	})
